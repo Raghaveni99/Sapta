19a659c2bfa3fa8ebcbc86c27a0251ea6eefc1d8
Raghaveni99 <68287473+Raghaveni99@users.noreply.github.com>
2020-07-15 20:03:37 +0530

    Update Demo1.java

--- src/a/Demo1.java
+++ src/a/Demo1.java
@@ -5,6 +5,9 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("A");
+		System.out.println("B");
+		System.out.println("A");System.out.println("A");System.out.println("A");
+		
 
 	}
 
